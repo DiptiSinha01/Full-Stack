f363ff4d9e9622198cfb706bf537b77eed8d0f65
DiptiSinha01 <diptisinha76h@gmail.com>
2026-04-02 10:17:53 +0530

    Added REST-SAMPLE project

---
+++ Experiments/Rest-sample/src/test/java/com/sample/Rest/sample/RestSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sample.Rest.sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
